1198472a9c59d955acc8cfc95816408926b00965
Cho Sang Jun <fpdltm456@gmail.com>
2024-01-16 01:30:48 +0900

    newproject(bp->c++)

---
+++ unreal/Project/Source/Project/ProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProjectGameMode();
+};
+
+
+
