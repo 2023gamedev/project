7baf5f3149378b34c9dcbb6e0c14ac09191665eb
Lee Han Joo <rndrnfptb@gmail.com>
2024-02-01 23:41:30 +0900

    동기화용 GameSyncManager 클래스 추가

---
+++ unreal/Server/DummyClient/Source/DummyClient/GameSyncManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class DUMMYCLIENT_API GameSyncManager
+{
+public:
+	GameSyncManager();
+	~GameSyncManager();
+
+	void Player();
+};
