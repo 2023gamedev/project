63a4636f761fdb5d1b71d366bb20f8076c365dc6
Cho Sang Jun <fpdltm456@gmail.com>
2024-02-03 22:22:19 +0900

    ai3

---
+++ unreal/Project/Source/Project/BTTask_Attack.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTTask_Attack.h"
+
+UBTTask_Attack::UBTTask_Attack()
+{
+	bNotifyTick = true;
+}
+
+EBTNodeResult::Type UBTTask_Attack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	EBTNodeResult::Type Result = Super::ExecuteTask(OwnerComp, NodeMemory);
+	return EBTNodeResult::InProgress;
+}
+
+void UBTTask_Attack::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickTask(OwnerComp, NodeMemory, DeltaSeconds);
+	FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+}
