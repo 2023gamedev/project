55ee336f2dcc8d989cc58ddebea159ced6974a04
Dooohooo <rlatkdduq9@gmail.com>
2024-02-27 09:47:20 +0900

    wtf

---
+++ unreal/Project/Source/Project/ItemDataStructure.h
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ItemDataStructure.generated.h"
+
+/**
+ * 
+ */
+class PROJECT_API ItemDataStructure
+{
+public:
+	ItemDataStructure();
+	~ItemDataStructure();
+};
+
+UENUM(BlueprintType)
+enum class EItemType
+{
+	ITEM_NONE			UMETA(DisplayName = "None"),
+	ITEM_USEABLE		UMETA(DisplayName = "Useable"),
+	ITEM_EQUIPMENT		UMETA(DisplayName = "Equipment")
+};
+
+USTRUCT(Atomic, BlueprintType)
+struct FItemDataStructure
+{
+
+	GENERATED_USTRUCT_BODY()
+
+public:
+
+
+	// virtual void Clear();
+	// virtual void Use(APlyaerCharacter* player) {};
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName Name;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UTexture2D* Texture;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TEnumAsByte<EItemType> Type;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int Count;
+};
+
+UENUM(BlueprintType)
+enum class ESlotType
+{
+	SLOT_NONE				UMETA(DisplayName = "None"),
+	SLOT_ITEM				UMETA(DisplayName = "Item"),
+	SLOT_QUICK				UMETA(DisplayName = "Quick"),
+	SLOT_QUICK_ITEM			UMETA(DisplayName = "Item"),
+};
---
+++ unreal/Project/Source/Project/StaminaBar.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StaminaBar.h"
+
+UStaminaBar::UStaminaBar(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+	m_fCurrentStamina = 100.f;
+	m_fMaxStamina	  = 100.f;
+}
+
+void UStaminaBar::UpdateStaminaBar(float currenstamina)
+{
+	m_fCurrentStamina = currenstamina;
+}
