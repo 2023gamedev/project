7df3785b1f43e2a681458ad36715176fbd328f34
Lee Han Joo <rndrnfptb@gmail.com>
2024-03-16 23:47:41 +0900

    Iocp Core, Iocp Event 모듈화

--- unreal/Server/Game Server/Server/Types.h
+++ unreal/Server/Game Server/Server/Types.h
@@ -12,16 +12,20 @@ using uint16 = unsigned __int16;
 using uint32 = unsigned __int32;
 using uint64 = unsigned __int64;
 
-template<typename T>
-using Atomic = std::atomic<T>;
-using Mutex = std::mutex;
-using CondVar = std::condition_variable;
-using UniqueLock = std::unique_lock<std::mutex>;
-using LockGuard = std::lock_guard<std::mutex>;
+#define USING_SHARED_PTR(name)	using name##Ref = std::shared_ptr<class name>;
+
+USING_SHARED_PTR(IocpCore);
+USING_SHARED_PTR(IocpObject);
+USING_SHARED_PTR(Session);
+USING_SHARED_PTR(PacketSession);
+USING_SHARED_PTR(Listener);
+USING_SHARED_PTR(ServerService);
+USING_SHARED_PTR(ClientService);
+USING_SHARED_PTR(SendBuffer);
+USING_SHARED_PTR(Job);
+USING_SHARED_PTR(JobQueue);
 
 #define size16(val)		static_cast<int16>(sizeof(val))
 #define size32(val)		static_cast<int32>(sizeof(val))
 #define len16(arr)		static_cast<int16>(sizeof(arr)/sizeof(arr[0]))
 #define len32(arr)		static_cast<int32>(sizeof(arr)/sizeof(arr[0]))
-
-#define _STOMP
