4afdc42103e0b1c56009c38fcc2f56e4915ce07e
Lee Han Joo <rndrnfptb@gmail.com>
2024-03-19 20:29:17 +0900

    테스트용 서버생성

---
+++ unreal/test/Server/DummyClient/Source/DummyClient/DummyClientGameMode.h
@@ -0,0 +1,27 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ClientSocket.h"
+#include "OtherCharacter.h"
+#include "DummyClientGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADummyClientGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADummyClientGameMode();
+
+	void UpdateOtherPlayer(uint32 PlayerID, FVector NewLocation, FRotator NewRotation);
+
+protected:
+	UPROPERTY()
+		TSubclassOf<ACharacter> BP_OtherCharacterClass;
+};
+
+
+
