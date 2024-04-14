c47683f300d8da4753e69a8ffd99e4a6464b3c38
Cho Sang Jun <fpdltm456@gmail.com>
2024-04-15 00:17:13 +0900

    asset3

--- unreal/Project/Source/Project/Private/ProItem/NWWoodenBat.cpp
+++ unreal/Project/Source/Project/Private/ProItem/NWWoodenBat.cpp
@@ -10,7 +10,7 @@ ANWWoodenBat::ANWWoodenBat()
 	NormalWeapon = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("NORMALWEAPON"));
 	RootComponent = NormalWeapon;
 
-	static ConstructorHelpers::FObjectFinder<USkeletalMesh> SK_NORMALWEAPON(TEXT("/Game/Mesh/SM_SquareWood.SM_SquareWood"));
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> SK_NORMALWEAPON(TEXT("/Game/Safe_House/meshes/SM_woodenbat.SM_woodenbat"));
 	if (SK_NORMALWEAPON.Succeeded()) {
 		NormalWeapon->SetSkeletalMesh(SK_NORMALWEAPON.Object);
 	}
