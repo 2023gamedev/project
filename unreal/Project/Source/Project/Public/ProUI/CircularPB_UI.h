ab1c41ce4d02c3c560373c6e88a5290996485c77
SeungWon-git <seungwon604@gmail.com>
2024-07-07 22:47:46 +0900

    2주차 작업

---
+++ unreal/Project/Source/Project/Public/ProUI/CircularPB_UI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CircularPB_UI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_API UCircularPB_UI : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
