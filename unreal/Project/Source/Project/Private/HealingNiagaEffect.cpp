c981b4047b79e927fb0693c3b38d33bb836ddcba
SeungWon-git <seungwon604@gmail.com>
2024-07-27 23:52:17 +0900

    나이아가라 이펙트 소스 파일 작성

---
+++ unreal/Project/Source/Project/Private/HealingNiagaEffect.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HealingNiagaEffect.h"
+
+// Sets default values
+AHealingNiagaEffect::AHealingNiagaEffect()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AHealingNiagaEffect::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHealingNiagaEffect::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
