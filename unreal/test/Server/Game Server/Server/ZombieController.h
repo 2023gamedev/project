c0672b0604e105cf451ef024097d14e62d9a80f6
Lee Han Joo <rndrnfptb@gmail.com>
2024-08-21 13:09:59 +0900

    좀비 데이터 서버로 이관

--- unreal/test/Server/Game Server/Server/ZombieController.h
+++ unreal/test/Server/Game Server/Server/ZombieController.h
@@ -1,10 +1,46 @@
 #pragma once
 #include "Common.h"
+#include "iocpServerClass.h"
 
 struct ZombieData {
 	int zombieID;
 	float x, y, z;
 	float pitch, yaw, roll;
+	int zombietype;
+	int patroltype;
+	float patrolrange;
+};
+
+struct Vector3 {
+	float X;
+	float Y;
+	float Z;
+
+	Vector3() : X(0), Y(0), Z(0) {}
+	Vector3(float x, float y, float z) : X(x), Y(y), Z(z) {}
+};
+
+struct Rotator {
+	float Pitch;
+	float Yaw;
+	float Roll;
+
+	Rotator() : Pitch(0), Yaw(0), Roll(0) {}
+	Rotator(float pitch, float yaw, float roll) : Pitch(pitch), Yaw(yaw), Roll(roll) {}
+};
+
+enum class EZombie
+{
+	NORMAL,
+	SHOUTING,
+	RUNNING,
+};
+
+enum class EZombiePatrol
+{
+	NOTPATROL,
+	PATROLX,
+	PATROLY,
 };
 
 class ZombieController
@@ -13,9 +49,10 @@ public:
 	ZombieController();
 	~ZombieController();
 
+	void SpawnZombies(int zombieID, int zombieType, Vector3 position, Rotator rotation, int patrolType, float patrolRange);
+
 	vector<ZombieData> zombiedata;
 
-	void addZombie(ZombieData zombie);
 	void removeZombie(int zombieID);
 	void setZombiePosition(ZombieData zombie);
 	ZombieData* getZombiePosition(int zombieid);
