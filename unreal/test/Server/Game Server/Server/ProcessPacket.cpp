9959b774b81e6d8e99acb53330f73d476c47cd80
SeungWon-git <seungwon604@gmail.com>
2024-10-17 05:11:34 +0900

    SendItem 쪽 메모리 참조 오류 문제 일단 주석처리

--- unreal/test/Server/Game Server/Server/ProcessPacket.cpp
+++ unreal/test/Server/Game Server/Server/ProcessPacket.cpp
@@ -29,8 +29,8 @@ bool IOCP_CORE::IOCP_ProcessPacket(int id, Packet* buffer, int bufferSize) {
         clientInfo->isInGame = true;
         b_Timer = true;
         zombieclass->SendZombieData(id);
-        itemclass->SendItemData(id);
-        printf("SendZombieData!! Playerid=#%d\n", id);
+        //itemclass->SendItemData(id);
+        //printf("SendZombieData!! Playerid=#%d\n", id);
     }
 
     // 패킷의 타입을 확인하여 처리
