a7ef402d42c55ad66d8551d3a60bbafafbd04eea
Lee Han Joo <rndrnfptb@gmail.com>
2024-11-19 23:59:44 +0900

    로그인 오류 관련 수정

--- unreal/test/LobbyServer/LobbyServer/LoginManager.cpp
+++ unreal/test/LobbyServer/LobbyServer/LoginManager.cpp
@@ -12,7 +12,7 @@ LoginManager::~LoginManager()
 // 회원가입
 bool LoginManager::RegisterName(const string& username, const string& password)
 {
-    LoadFromFile("users.txt");
+    std::lock_guard<std::mutex> lock(login_mutex);
     for (const auto& user : users) {
         if (user.username == username) {
             cout << "Username already exists." << endl;
@@ -28,9 +28,15 @@ bool LoginManager::RegisterName(const string& username, const string& password)
 // 로그인
 bool LoginManager::Login(const string& username, const string& password)
 {
-    LoadFromFile("users.txt");
+    if (logged_in_users[username]) {
+        cout << "이미 로그인된 사용자입니다." << endl;
+        return false;
+    }
+
+    std::lock_guard<std::mutex> lock(login_mutex);
     for (const auto& user : users) {
         if (user.username == username && user.password == password) {
+            logged_in_users[username] = true;
             return true;
         }
     }
@@ -39,9 +45,21 @@ bool LoginManager::Login(const string& username, const string& password)
     return false;
 }
 
+bool LoginManager::Logout(const string& username)
+{
+    if (logged_in_users.find(username) != logged_in_users.end() && logged_in_users[username]) {
+        logged_in_users[username] = false;
+        cout << username << "님이 로그아웃했습니다." << endl;
+        return true;
+    }
+    cout << "로그인 상태가 아닙니다." << endl;
+    return false;
+}
+
 // 유저 정보 저장
 void LoginManager::SaveToUser(const string& filename, const string& username, const string& password)
 {
+    users.push_back({ username, password });
     ofstream file(filename, ios::app);
     if (file.is_open()) {
         file << username << " " << password << endl;
@@ -52,6 +70,7 @@ void LoginManager::SaveToUser(const string& filename, const string& username, co
 // 파일 불러오기
 void LoginManager::LoadFromFile(const string& filename)
 {
+    users.clear();
     ifstream file(filename);
     if (file.is_open()) {
         string username, password;
