51300622290617521fd68f5eb4a09efdcc72b11d
Cho Sang Jun <fpdltm456@gmail.com>
2024-11-24 15:53:00 +0900

    헬리콥터 떠나는 사운드, UnLock 사운드 추가

--- unreal/Project/Source/Project/Private/ProCharacter/BaseCharacter.cpp
+++ unreal/Project/Source/Project/Private/ProCharacter/BaseCharacter.cpp
@@ -770,7 +770,7 @@ void ABaseCharacter::UpdatePickUpKey(uint32 keyid, uint32 playerid)
 		Cast<UTextMissionUI>(TextMissionUIWidget)->PlayFadeOutMT1();
 	}
 	else if (keyid == 2 && TextMissionUIWidget->m_iFindRoofKey == 2) {
-		FText KText = FText::FromString(TEXT("옥상 키를 찾았습니다."));
+		FText KText = FText::FromString(TEXT("옥상 키를 모두 찾았습니다."));
 		USoundBase* Sound = LoadObject<USoundBase>(nullptr, TEXT("/Game/Sound/ChartFindKey.ChartFindKey")); // 에셋 경로
 		PlaySoundForPlayer(Sound);
 		ShowActionText(KText, FSlateColor(FLinearColor(1.0f, 1.0f, 1.0f)), 5.f);
@@ -1508,6 +1508,8 @@ void ABaseCharacter::KeyMontageEnded(UAnimMontage* Montage, bool bInterrupted)
 		if (CarActor && CurrentKeyItem->KeyName == CarActor->CarKeyName) {
 			CarActor->UnLock();
 			GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Green, "Key UNLOCK!!!");
+			USoundBase* Sound = LoadObject<USoundBase>(nullptr, TEXT("/Game/Sound/UnLockDoor.UnLockDoor")); // 에셋 경로
+			PlaySoundForPlayer(Sound);
 			UpdateKeySlot();
 			Send_OpenRoot(1);
 		}
@@ -1521,12 +1523,16 @@ void ABaseCharacter::KeyMontageEnded(UAnimMontage* Montage, bool bInterrupted)
 			if (CurrentKeyItem->KeyName == "RoofKey1") {
 				RoofTopDoorActor->UnlockKey1();
 				GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Green, "ROOFKEY1 UnLock");
+				USoundBase* Sound = LoadObject<USoundBase>(nullptr, TEXT("/Game/Sound/UnLockDoor.UnLockDoor")); // 에셋 경로
+				PlaySoundForPlayer(Sound);
 				UpdateKeySlot();
 				Send_OpenRoot(2);
 			}
 			else if (CurrentKeyItem->KeyName == "RoofKey2") {
 				RoofTopDoorActor->UnlockKey2();
 				GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Green, "ROOFKey2 Unlock");
+				USoundBase* Sound = LoadObject<USoundBase>(nullptr, TEXT("/Game/Sound/UnLockDoor.UnLockDoor")); // 에셋 경로
+				PlaySoundForPlayer(Sound);
 				UpdateKeySlot();
 				Send_OpenRoot(2);
 			}
