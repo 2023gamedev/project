032e31c01cac762c623e03a2074dc935033774a2
SeungWon-git <seungwon604@gmail.com>
2024-12-04 20:04:41 +0900

    안 쓰는 폴더, 프로젝트 삭제 및 위치 조정 및 정리

--- unreal/Server/Game Server/Server/CoreGlobal.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-extern class ThreadManager* GThreadManager;
-extern class GlobalQueue* GGlobalQueue;
-extern class JobTimer* GJobTimer;
--- unreal/Server/Game Server/Server/CoreTLS.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <stack>
-
-extern thread_local uint32				LThreadId;
-extern thread_local uint64				LEndTickCount;
-
-extern thread_local class JobQueue* LCurrentJobQueue;
