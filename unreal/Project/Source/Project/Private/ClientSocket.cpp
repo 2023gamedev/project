10553a421f948a779df60abcf6dfd7e2118d76ea
Lee Han Joo <rndrnfptb@gmail.com>
2024-12-21 15:47:58 +0900

    slicevector recv 수정

--- unreal/Project/Source/Project/Private/ClientSocket.cpp
+++ unreal/Project/Source/Project/Private/ClientSocket.cpp
@@ -558,8 +558,8 @@ void ClientSocket::ProcessPacket(const std::vector<char>& buffer)
 
 					slicevector.location = FVector(slicepacket.location().x(), slicepacket.location().y(), slicepacket.location().z());
 					slicevector.position = FVector(slicepacket.position().x(), slicepacket.position().y(), slicepacket.position().z());
-					slicevector.location = FVector(slicepacket.normal().x(), slicepacket.normal().y(), slicepacket.normal().z());
-					slicevector.location = FVector(slicepacket.impulse().x(), slicepacket.impulse().y(), slicepacket.impulse().z());
+					slicevector.normal = FVector(slicepacket.normal().x(), slicepacket.normal().y(), slicepacket.normal().z());
+					slicevector.impulse = FVector(slicepacket.impulse().x(), slicepacket.impulse().y(), slicepacket.impulse().z());
 
 					Q_slicevector.push(slicevector);
 
