fe2f56c3080b11bacfe2ad1aa7b20f4ac1d29b55
Cho Sang Jun <fpdltm456@gmail.com>
2024-12-22 15:50:04 +0900

    runningzombie 소리 두번들리는 부분 수정

--- unreal/Project/Source/Project/Private/ProZombie/BaseZombie.cpp
+++ unreal/Project/Source/Project/Private/ProZombie/BaseZombie.cpp
@@ -424,9 +424,10 @@ float ABaseZombie::TakeDamage(float DamageAmount, FDamageEvent const& DamageEven
 
 			BloodFX.Add(NewBloodFX);
 		}
+
+		BeAttacked();
 	}
 
-	BeAttacked();
 
 	return Damage;
 }
