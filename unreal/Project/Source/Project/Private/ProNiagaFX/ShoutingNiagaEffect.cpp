d37cc74dd1fd317bc72ba5807b878ea5c34bd791
SeungWon-git <seungwon604@gmail.com>
2024-12-30 21:12:28 +0900

    GEngine 잘못된 메모리 참조 오류로 에디터 안 열리던 현상 수정

--- unreal/Project/Source/Project/Private/ProNiagaFX/ShoutingNiagaEffect.cpp
+++ unreal/Project/Source/Project/Private/ProNiagaFX/ShoutingNiagaEffect.cpp
@@ -2,6 +2,7 @@
 
 
 #include "ProNiagaFX/ShoutingNiagaEffect.h"
+
 #include "Components/WidgetComponent.h"
 #include "NiagaraFunctionLibrary.h"
 //#include "ProZombie/BaseZombie.h"
@@ -16,13 +17,15 @@ AShoutingNiagaEffect::AShoutingNiagaEffect()
  
 	RootComponent = Mesh;
 
-	GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Blue, "Shouting FX generated");
+	//GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Blue, "Shouting FX generated");	// 게임 초기화 전에 부르면 메모리 엑세스 오류 일으킴 (최소 BeginPlay 이후에 불러야함)
+	UE_LOG(LogTemp, Log, TEXT("Shouting FX generated"));
 
 	static ConstructorHelpers::FObjectFinder<UNiagaraSystem> NE(TEXT("/Script/Niagara.NiagaraSystem'/Game/ShoutingEffect.ShoutingEffect'"));
 	if (NE.Succeeded())
 	{
 		ShoutingFXSystem = NE.Object;
-		GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Blue, "Shouting FX System found");
+		//GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Blue, "Shouting FX System found");
+		UE_LOG(LogTemp, Log, TEXT("Shouting FX System found"));
 	}
 }
 
